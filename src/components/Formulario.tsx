b286d5a93416d2aaa4f0c1a1b8dce445c4cc3ec5
Lilian1306 <kbts2207@gmail.com>
2025-11-18 18:00:26 -0600

    Agregando un background en la pagina principal

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -14,9 +14,6 @@ export default function Formulario() {
    const handleSubmit = (e: React.FormEvent) => {
      e.preventDefault()
      const memberFound = Member.find((m) => m.name === cards)
-
-    
-
      setEnviar({name, country, cards, image: memberFound ? memberFound?.image : ""})
   }
 
@@ -24,28 +21,28 @@ export default function Formulario() {
     <div className=" mt-20">
         {!enviar ? (    
         <form 
-          className="items-center mt-10 flex flex-col bg-purple-200 shadow-xl rounded-lg p-10 w-96 mx-auto"
+          className="items-center mt-10 flex flex-col bg-purple-500 shadow-xl rounded-lg p-10 w-96 mx-auto"
           onSubmit={handleSubmit}
         >
         
-          <label htmlFor="name" className=" text-gray-700"
+          <label htmlFor="name" className=" text-white"
           >Escriba tu nombre: </label>
           <input
            type="text"
            id="name"
            placeholder="Escribe tu nombre aqui"
-           className="border border-gray-500 rounded-lg p-2 w-full"
+           className="border border-gray-900 rounded-lg p-2 w-full "
            value={name}
            onChange={(e) => setName(e.target.value)}
           />
 
-          <label htmlFor="country" className="text-gray-700 mt-5"
+          <label htmlFor="country" className="text-white mt-5"
           >Escribe tu ciudad:</label>
           <input
             type="text"
             id="country"
             placeholder="Escribe tu ciudad aqui"
-            className="border border-gray-500 rounded-lg p-2 w-full"
+            className="border border-gray-900 rounded-lg p-2 w-full"
             value={country} 
             onChange={(e) => setCountry(e.target.value)}
           />
@@ -54,7 +51,7 @@ export default function Formulario() {
         
           <select
              id="cards"
-             className="border border-gray-500 rounded-lg w-60 mt-5 p-2"
+             className="border border-gray-900 rounded-lg w-60 mt-5 p-2 text-black"
              value={cards}
              onChange={(e) => setCards(e.target.value)}
           >
@@ -73,7 +70,7 @@ export default function Formulario() {
           </div>
           <button
             type="submit"
-            className="mt-5 rounded rounded-xl w-full cursor-pointer p-2 bg-purple-400 text-purple-900 font-bold shadow-xl"
+            className="mt-5 rounded rounded-xl w-full cursor-pointer p-2 bg-purple-400 text-white font-bold shadow-xl"
           >Enviar</button>
         </form>
   
